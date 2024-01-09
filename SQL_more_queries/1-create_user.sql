8f1898526d10a673e2e394d31dae160297a78f3e
sfanxAK <soufianeakhiat@gmail.com>
2024-01-09 21:30:46 +0100

    add

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -3,4 +3,4 @@ CREATE USER [IF NOT EXISTS] user_0d_1
 -- Set password for user_0d_1
 IDENTIFIED BY 'user_0d_1_pwd';
 -- user_0d_1 has all privileges
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
